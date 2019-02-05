995886e20b5fa35d43b8f8b8047d5066cce4a143
kkoomin <minha.koo.9@gmail.com>
2019-02-05 15:12:36 +0000

    Do not trust strong params

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,47 @@
 class EmployeesController < ApplicationController
+    before_action :find_employee, only: [:show, :edit, :update]
+    
+    def index
+        @employees = Employee.all
+    end
+    
+    def show
+    end
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+
+    end
+
+    def edit
+        @dogs = Dog.all
+    end
+
+    def update
+        @employee.update(employee_params)
+        redirect_to employee_path(@employee)
+    end
+
+    private
+
+    def find_employee
+        @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+        params.require(:employee).permit(
+            :first_name,
+            :last_name,
+            :alias,
+            :title,
+            :office,
+            :img_url,
+            :dog_id
+        )
+    end
 end
